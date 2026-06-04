4e377fef3132d81dcac1fd3f10f762a445867d22
kashc <kashc@THE_BEAST>
2026-06-04 14:21:41 +0200

    Basic front end elements in place. proceeding to backend code

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using Newtonsoft.Json;
+using System.Net;
+
 namespace myWeatherApplication
 {
     public partial class Form1 : Form
@@ -15,6 +19,62 @@ namespace myWeatherApplication
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string APIKey = "c4b007fac292ef6cb7a42f910bb1e6dc";
+        void getWeather()
+        {
+            using (WebClient web = new WebClient())
+            { 
+            
+            
+            
+            }
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labSunrise_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
