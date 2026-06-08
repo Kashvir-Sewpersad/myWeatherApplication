2583538bc4a65ce8faf16fe0acd0bfda554998fc
kashc <kashc@THE_BEAST>
2026-06-08 14:11:22 +0200

    completed base functionality. proceeding with improvements

--- Form1.cs
+++ Form1.cs
@@ -25,10 +25,19 @@ namespace myWeatherApplication
         void getWeather()
         {
             using (WebClient web = new WebClient())
-            { 
-            
-            
-            
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}", TBCity.Text, APIKey);
+                var jason = web.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(jason);
+
+                picIcon.ImageLocation = "https://openweathermap.org/img/w/" + Info.weather[0].icon + ".png";
+                labCondition.Text = Info.weather[0].main;
+                labDetails.Text = Info.weather[0].description;
+                labSunset.Text = convertDateTime(Info.sys.sunset).ToShortTimeString();
+                labSunrise.Text = convertDateTime(Info.sys.sunrise).ToShortTimeString();
+                labWindSpeed.Text = Info.wind.speed.ToString();
+                labPressure.Text = Info.main.pressure.ToString();
+
             }
         
         
@@ -36,7 +45,13 @@ namespace myWeatherApplication
         
         }
 
+        DateTime convertDateTime(long millisec)
+        {
+            DateTime day = new DateTime(1970, 1, 1, 0,0, 0, 0, System.DateTimeKind.Utc).ToLocalTime();
+            day = day.AddSeconds(millisec).ToLocalTime();
+            return day;
 
+        }
 
 
 
@@ -74,7 +89,7 @@ namespace myWeatherApplication
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-
+            getWeather();
         }
     }
 }
